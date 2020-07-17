769405432014dfafac58bd0ec7d1b5ab5f50e9db
冯兵兵 <fengbingbing@iqianjin.com>
2020-07-17 18:37:34 +0800

    第七章：Spring Cloud Gateway入门

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # spring-cloud-tutorials
 Spring Cloud 教程
 
+```
+Release Train Version: Hoxton.SR5
+Supported Boot Version: Spring Boot >=2.2.0.M4 and <2.3.2.BUILD-SNAPSHOT
+```
+
 ## Documents
 * [Spring Cloud Hoxton.SR5官方文档](https://cloud.spring.io/spring-cloud-static/Hoxton.SR5/reference/html/)
 
---
+++ spring-cloud-gateway-demo/src/main/java/io/dreamstudio/springcloud/gateway/demo/config/GatewayConfig.java
@@ -0,0 +1,31 @@
+package io.dreamstudio.springcloud.gateway.demo.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Ricky Fung
+ */
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/user/getByUsername")
+                        .uri("http://localhost:8080/user/getByUsername"))
+                .build();
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://localhost:8080"))
+                .build();
+    }
+}
