fccdc88e8d37fa06e99af62968d97412293261f0
冯兵兵 <fengbingbing@iqianjin.com>
2020-07-20 16:14:58 +0800

    第八章：Spring Cloud 服务容错保护Hystrix

---
+++ spring-cloud-netflix-hystrix/spring-cloud-netflix-hystrix-demo/src/main/java/io/dreamstudio/springcloud/netflix/hystrix/HystrixApplication.java
@@ -0,0 +1,19 @@
+package io.dreamstudio.springcloud.netflix.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+/**
+ * @author Ricky Fung
+ */
+@EnableHystrix
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class, args);
+    }
+}
--- spring-cloud-netflix-zuul/spring-cloud-netflix-zuul-user-service/src/main/java/io/dreamstudio/springcloud/netflix/zuul/user/controller/UserApiController.java
+++ spring-cloud-netflix-zuul/spring-cloud-netflix-zuul-user-service/src/main/java/io/dreamstudio/springcloud/netflix/zuul/user/controller/UserApiController.java
@@ -1,10 +1,15 @@
 package io.dreamstudio.springcloud.netflix.zuul.user.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Ricky Fung
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class UserApiController {
+    private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
     @Value("${server.port:}")
     private Integer serverPort;
@@ -22,5 +28,16 @@ public class UserApiController {
     @GetMapping("/echo/{message}")
     public String echo(@PathVariable("message") String message) {
         return String.format("[%s : %s] hello %s", applicationName, serverPort, message);
+    }
+
+    @GetMapping("/users/{id}")
+    public String getUserById(@PathVariable("id") Long id) throws InterruptedException {
+        int sleep = new Random().nextInt(8);
+        if (sleep < 1) {
+            sleep = 1;
+        }
+        LOG.info("本次调用sleep:{}", sleep);
+        TimeUnit.SECONDS.sleep(sleep);
+        return String.format("[%s : %s] getUserById %s", applicationName, serverPort, id);
     }
 }
