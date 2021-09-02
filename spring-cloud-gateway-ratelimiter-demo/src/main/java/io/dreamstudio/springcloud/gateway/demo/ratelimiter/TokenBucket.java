6b83fc1c97dcf0f4197a9a2f5bc826483a20d227
冯兵兵 <fengbingbing@iqianjin.com>
2021-09-02 10:43:18 +0800

    第十三章：Spring Cloud Gateway 限流示例

--- spring-cloud-gateway-ratelimiter-demo/src/main/java/io/dreamstudio/springcloud/gateway/demo/ratelimiter/TokenBucket.java
+++ spring-cloud-gateway-ratelimiter-demo/src/main/java/io/dreamstudio/springcloud/gateway/demo/ratelimiter/TokenBucket.java
@@ -4,6 +4,9 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 令牌桶算法
+ * 一般会有两种不同的实现方式：
+ * 1. 第一种方式是启动一个内部线程，不断的往桶中添加令牌，处理请求时从桶中获取令牌，和上面图中的处理逻辑一样。
+ * 2. 第二种方式不依赖于内部线程，而是在每次处理请求之前先实时计算出要填充的令牌数并填充，然后再从桶中获取令牌。
  * @author Ricky Fung
  */
 public class TokenBucket {
